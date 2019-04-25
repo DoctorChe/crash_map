f08a90d5e134933c1a1902c235c81ac1745cd8b9
DoctorChe <duncan.reg@yandex.ru>
2019-04-25 17:34:41 +0300

    start work on the parser (2)

--- back_end/gbdd_json_to_csv.py
+++ back_end/gbdd_json_to_csv.py
@@ -1,27 +1,3 @@
-# import csv
-# import json
-#
-#
-# def flattenjson(b, delim):
-#     val = {}
-#     for i in b.keys():
-#         if isinstance(b[i], dict):
-#             get = flattenjson(b[i], delim)
-#             for j in get.keys():
-#                 val[i + delim + j] = get[j]
-#         else:
-#             val[i] = b[i]
-#
-#     return val
-#
-#
-# with open(fname, 'wb') as out_file:
-#     csv_w = csv.writer(out_file)
-#     csv_w.writerow(columns)
-#
-#     for i_r in input:
-#         csv_w.writerow(map(lambda x: i_r.get(x, ""), columns))
-
 import csv
 import json
 import sys
@@ -29,35 +5,65 @@ import sys
 # if you are not using utf-8 files, remove the next line
 # sys.setdefaultencoding("UTF-8")  # set the encode to utf8
 # check if you pass the input file and output file
+from pprint import pprint
+
 if sys.argv[1] is not None and sys.argv[2] is not None:
     fileInput = sys.argv[1]
     fileOutput = sys.argv[2]
     inputFile = open(fileInput, "r", encoding="utf-8")  # open json file
-    outputFile = open(fileOutput, "w")  # load csv file
-    # data = json.load(inputFile)  # load json content
-    data = json.load(inputFile)["data"]  # load json content
-    # data = json.load(inputFile)["data"]["cards"]  # load json content
-    print(data)
-    print(data[0])
+    data = json.load(inputFile)  # load json content
     inputFile.close()  # close the input file
 
-    year = 2018
-    region = {
-        "id": 50,
-        "name": "Новосибирская область",
+    cards = json.loads(data["data"])["cards"]
+    pprint(cards[0])
+
+    card = cards[0]
+
+    dtp_dict = {
+        "DATE": card['date'],
+        "TIME": card['Time'],
+        "TYPE": card['DTP_V'],
+        "LOCATION": card['District'],
+        "STREET": card['infoDtp']['k_ul'],
+        "HOUSE_NUMBER": card['infoDtp']['house'],
+        "ROAD": card['infoDtp']['dor'],
+        "KILOMETER": card['infoDtp']['km'],
+        "METER": card['infoDtp']['m'],
+        "LONGITUDE": card['infoDtp']['COORD_L'],
+        "LATITUDE": card['infoDtp']['COORD_W'],
+        "DEATH": card['POG'],
+        # "DEATH_CHILDREN": card['date'],
+        "INJURY": card['RAN'],
+        # "INJURY_CHILDREN": card['date'],
+
+        # "LONGITUDE_GEOCODE": card['date'],
+        # "LATITUDE_GEOCODE": card['date'],
+        # "VALID": card['date'],
+        # "VALID_STRICT": card['date'],
     }
-    months = [3, 3]
 
-    dtp_dict = {"data": {}}
-    dtp_dict["data"]["year"] = str(year)
-    dtp_dict["data"]["region_code"] = region["id"]
-    dtp_dict["data"]["region_name"] = region["name"]
-    dtp_dict["data"]["month_first"] = months[0]
-    dtp_dict["data"]["month_last"] = months[-1]
+    pprint(dtp_dict)
 
-    dtp_dict["data"]["cards"] = []
+    # print(data[0])
 
-    cards = get_dtp_data(region["id"], region["name"], district["id"], district["name"], months, year)
+
+    # year = 2018
+    # region = {
+    #     "id": 50,
+    #     "name": "Новосибирская область",
+    # }
+    # months = [3, 3]
+    #
+    # dtp_dict = {"data": {}}
+    # dtp_dict["data"]["year"] = str(year)
+    # dtp_dict["data"]["region_code"] = region["id"]
+    # dtp_dict["data"]["region_name"] = region["name"]
+    # dtp_dict["data"]["month_first"] = months[0]
+    # dtp_dict["data"]["month_last"] = months[-1]
+    #
+    # dtp_dict["data"]["cards"] = []
+
+    # cards = get_dtp_data(region["id"], region["name"], district["id"], district["name"], months, year)
 
     # for card in cards:
     #     # получение карточек ДТП
@@ -70,6 +76,7 @@ if sys.argv[1] is not None and sys.argv[2] is not None:
     #     write_log(log_text)
     #     cards = get_dtp_data(region["id"], region["name"], district["id"], district["name"], months, year)
 
+    outputFile = open(fileOutput, "w")  # load csv file
     # output = csv.writer(outputFile)  # create a csv.write
     # # output.writerow(data[0].keys())  # header row
     # for row in data:
